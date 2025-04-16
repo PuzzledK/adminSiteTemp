e754e5c7a1729292e78019057d909681fac87697
Indra <ikhatri_be22@thapar.edu>
2025-04-16 22:48:16 +0530

    ,.

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/adminSiteTemp/',
   server: {
     host: true, // 👈 Allows access from any network (0.0.0.0)
     port: 5173, // You can change this to any port you prefer
     strictPort: true, // Ensures Vite uses this exact port
     cors: true, // Enable CORS (if needed)
     allowedHosts:true,
-    base: '/adminSiteTemp/'
   }
 })
